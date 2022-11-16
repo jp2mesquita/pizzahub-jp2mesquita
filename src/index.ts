359cad23638180e902598902c8a9f94fc016f121
João Paulo Mesquita <jp-mesquita@live.com>
2022-11-16 15:12:10 -0300

    feat: add the router to index

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import path from 'node:path';
 require('dotenv/config');
+
+import { router } from './router';
 
 const MONGODB_SECRET_CONECTION_KEY = process.env.MONGODB_SECRET_CONECTION_KEY as string;
 
@@ -8,6 +11,10 @@ mongoose.connect(MONGODB_SECRET_CONECTION_KEY)
   .then(() => {
     const app = express();
     const port = 3333;
+
+    app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
+    app.use(express.json());
+    app.use(router);
 
     console.log('conectado ao mongo');
     app.listen(port, () => {
