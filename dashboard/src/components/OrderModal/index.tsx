794f0e9b0f447c8a0062c076f6ff3d122983851b
João Paulo Mesquita <jp-mesquita@live.com>
2022-11-17 18:10:12 -0300

    feat: adding the content of modal, add esc feat

--- dashboard/src/components/OrderModal/index.tsx
+++ dashboard/src/components/OrderModal/index.tsx
@@ -1,18 +1,113 @@
-import { Overlay } from './styles';
+import { Actions, ModalBody, OrderDetails, Overlay } from './styles';
+
+import closeIcon from '../../assets/images/close-icon.svg';
+import { Order } from '../../@types/Order';
+import { formatCurrency } from '../../utils/formatCurency';
+import { useEffect } from 'react';
 
 interface OrderModalProps{
   visible: boolean
+  order: Order | null
+  onClose: () => void
 }
 
-export function OrderModal({visible}: OrderModalProps){
 
-  if(!visible){
+export function OrderModal({visible, order, onClose}: OrderModalProps){
+
+  useEffect(() => {
+    function handleKeyDown(event: KeyboardEvent){
+      if (event.key === 'Escape'){
+        onClose();
+      }
+    }
+
+    document.addEventListener('keydown', handleKeyDown);
+
+    return() => {
+      document.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [onClose]);
+
+
+  if(!visible || !order){
     return null;
   }
 
+  const total = order?.products.reduce((total, {product, quantity}) => {
+    return total += (product.price * quantity);
+  }, 0);
+
   return(
     <Overlay>
-      <h1>Modal</h1>
+      <ModalBody>
+        <header>
+          <strong>Mesa {order.table}</strong>
+          <button type='button' onClick={onClose}>
+            <img src={closeIcon} alt="Close Icon" />
+          </button>
+        </header>
+
+        <div className='status-container'>
+          <small>Status do Pedido</small>
+          <div>
+            <span>
+              {order.status === 'WAITING' && '⏱️'}
+              {order.status === 'IN_PRODUCTION' && '🧑‍🍳'}
+              {order.status === 'DONE' && '✅'}
+            </span>
+            <strong>
+              {order.status === 'WAITING' && 'Fila de espear'}
+              {order.status === 'IN_PRODUCTION' && 'Em preparação'}
+              {order.status === 'DONE' && 'Pronto'}
+            </strong>
+          </div>
+        </div>
+
+        <OrderDetails>
+          <strong>Items</strong>
+
+          <div className='order-items'>
+            {order.products.map(({_id, product, quantity}) => (
+              <div
+                className='item'
+                key={_id}
+              >
+                <picture>
+                  <img
+                    src={`http://localhost:3333/uploads/${product.imagePath}`}
+                    alt={product.name}
+
+                  />
+                </picture>
+
+                <span className='quantity'>{quantity}x</span>
+
+                <div className='product-details'>
+                  <strong>{product.name}</strong>
+                  <span>{formatCurrency(product.price).toUpperCase()}</span>
+                </div>
+              </div>
+            ))}
+          </div>
+
+          <div className='total'>
+            <span>Total</span>
+            <strong>{formatCurrency(total as number)}</strong>
+
+          </div>
+        </OrderDetails>
+
+        <Actions>
+          <button type='button' className='primary'>
+            <span>👩🏽‍🍳</span>
+            <span>Iniciar Produção</span>
+          </button>
+
+          <button type='button' className='secondary'>
+            <span>Cancelar Pedido</span>
+          </button>
+        </Actions>
+      </ModalBody>
     </Overlay>
   );
 }
--- dashboard/src/components/OrderModal/styles.ts
+++ dashboard/src/components/OrderModal/styles.ts
@@ -8,4 +8,152 @@ export const Overlay = styled.div`
   top: 0px;
   background: rgba( 0, 0, 0, 0.8);
   backdrop-filter: blur(4.5px);
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+
+export const ModalBody = styled.div`
+  background: #fff;
+  width: 30rem;
+  padding: 2rem;
+  border-radius: .5rem;
+
+  header{
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    strong{
+      font-size: 2.5rem;
+    }
+    button{
+      line-height: 0;
+      border: 0;
+      background: transparent;
+    }
+  }
+
+  .status-container{
+    margin-top:  2rem;
+
+    small{
+      font-size: .875rem;
+      opacity: .8;
+    }
+
+    div{
+      margin-top: .5rem;
+      display: flex;
+      align-items: center;
+      gap: .8rem;
+
+    }
+  }
+
+
+`;
+
+export const OrderDetails = styled.div`
+  margin-top: 2rem;
+
+  > strong{
+    font-weight: 500;
+    font-size: .875rem;
+    opacity: .8;
+  }
+
+  .order-items{
+    margin-top: 1rem;
+
+
+    .item{
+      display: flex;
+
+
+      & + .item{
+        margin-top: 1rem;
+      }
+
+      picture{
+        width: 3.5rem;
+        height: 3rem;
+        overflow: hidden;
+        position: relative;
+        img{
+          position: absolute;
+          object-fit: cover;
+          width: 100%;
+          height: 100%;
+          border-radius: .5rem;
+        }
+      }
+
+
+      .quantity{
+        font-size: .875rem;
+        color: #666;
+        display: block;
+        min-width: 1.25rem;
+        margin-left: .75rem;
+
+      }
+
+      .product-details{
+        margin-left: .25rem;
+        strong{
+          display: block;
+          margin-bottom: .25rem;
+        }
+
+        span{
+          font-size: .875rem;
+          color: #666;
+        }
+      }
+    }
+  }
+
+  .total{
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin-top: 1.5rem;
+    span{
+      font-weight: 500;
+      font-size: .875rem;
+      opacity: .8;
+    }
+  }
+
+`;
+
+export const Actions = styled.footer`
+  display: flex;
+  flex-direction: column;
+  margin-top: 2rem;
+
+  .primary{
+    background-color: #333;
+    border-radius: 3rem;
+    border: 0;
+    color: #fff;
+    padding: .75rem 2.5rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: .5rem;
+
+  }
+
+  .secondary{
+    padding: .75rem 2.5rem;
+    color: #d73035;
+    font-weight: bold;
+    border: 0;
+    background: transparent;
+    margin-top: .75rem;
+  }
 `;
