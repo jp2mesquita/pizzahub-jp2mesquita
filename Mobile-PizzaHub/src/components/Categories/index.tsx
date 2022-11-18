fc16df9b8ad98771529fc61461b980421b52eae3
João Paulo Mesquita <jp-mesquita@live.com>
2022-11-18 13:02:06 -0300

    feat: add touchable interaction in categories

--- Mobile-PizzaHub/src/components/Categories/index.tsx
+++ Mobile-PizzaHub/src/components/Categories/index.tsx
@@ -1,9 +1,18 @@
+import { useState } from 'react';
 import { FlatList } from 'react-native';
 import { categories } from '../../mocks/categories';
 import { Text } from '../Text';
 import { Category, Icon } from './styles';
 
 export function Categories(){
+
+  const [selectedCategory, setSelectedCategory] = useState< string>('');
+
+  function handleSelectCategory(categoryId: string){
+    const cetegory = selectedCategory === categoryId ? '' : categoryId;
+    setSelectedCategory(cetegory);
+  }
+
   return (
 
     <FlatList
@@ -12,18 +21,26 @@ export function Categories(){
       contentContainerStyle={{paddingRight: 24}}
       data={categories}
       keyExtractor={category => category._id}
-      renderItem={ ({item: category}) => (
-        <Category >
-          <Icon>
-            <Text>
-              <Text>{category.icon}</Text>
-            </Text>
-          </Icon>
+      renderItem={ ({item: category}) => {
+        const isSelected = selectedCategory === category._id;
 
-          <Text size={14} weight="600">{category.name}</Text>
-        </Category>
-      )
-      }
+        return(
+          <Category onPress={() => handleSelectCategory(category._id)}>
+            <Icon>
+
+              <Text opacity={isSelected ? 1 : .3}>
+                {category.icon}
+              </Text>
+
+            </Icon>
+
+            <Text size={14} weight="600" opacity={isSelected ? 1 : .3}>
+              {category.name}
+            </Text>
+          </Category>
+        );
+      }}
+
     >
 
     </FlatList>
