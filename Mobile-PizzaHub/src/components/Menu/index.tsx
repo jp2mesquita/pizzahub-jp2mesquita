e59a05c1b270e0593e62ad8772f7cf836e1abd6f
João Paulo Mesquita <jp-mesquita@live.com>
2022-11-18 15:24:37 -0300

    feat: create the content to menu  of items

--- Mobile-PizzaHub/src/components/Menu/index.tsx
+++ Mobile-PizzaHub/src/components/Menu/index.tsx
@@ -1,3 +1,47 @@
+import { FlatList } from 'react-native';
+import { products } from '../../mocks/products';
+import { formatCurrency } from '../../utils/formatCurency';
+import { PlusCircle } from '../Icons/PlusCircle';
+import { Text } from '../Text';
+
+import { Product, ProductImage, ProductDetails, Separator, AddToCartButton } from './styles';
+
 export function Menu(){
-  return null;
+  return (
+    <FlatList
+      ItemSeparatorComponent={Separator}
+      style={{marginTop: 32}}
+      contentContainerStyle={{paddingHorizontal: 24}}
+      data={products}
+      keyExtractor={product => product._id}
+      renderItem={({ item: product }) => (
+        <Product>
+          <ProductImage
+            source={{
+              uri: `http://192.168.1.5:3333/uploads/${product.imagePath}`,
+            }}
+          />
+          <ProductDetails>
+            <Text weight='600' style={{marginVertical: 8}}>
+              {product.name}
+            </Text>
+
+            <Text size={14} color="#666">
+              {product.description}
+            </Text>
+
+            <Text size={14} weight='600'>
+              {formatCurrency(product.price)}
+            </Text>
+          </ProductDetails>
+
+          <AddToCartButton>
+            <PlusCircle />
+          </AddToCartButton>
+
+        </Product>
+      )
+      }
+    />
+  );
 }
