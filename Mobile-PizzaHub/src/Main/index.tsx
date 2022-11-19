498c61c3254d689a57c16474c3427161f5a86f14
João Paulo Mesquita <jp-mesquita@live.com>
2022-11-19 17:05:31 -0300

    feat: change the header when a table is selected

--- Mobile-PizzaHub/src/Main/index.tsx
+++ Mobile-PizzaHub/src/Main/index.tsx
@@ -16,10 +16,17 @@ export function Main(){
     setSelectedTable(table);
   }
 
+  function handleCancelOrder(){
+    setSelectedTable('');
+  }
+
   return(
     <>
       <Container>
-        <Header />
+        <Header
+          selectedTable={selectedTable}
+          onCancelOrder={handleCancelOrder}
+        />
 
         <CategoriesContainer>
           <Categories />
