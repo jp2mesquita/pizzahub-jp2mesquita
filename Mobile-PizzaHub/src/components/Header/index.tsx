150149291d030ba53708d86cfc38985862fd344c
João Paulo Mesquita <jp-mesquita@live.com>
2022-11-19 17:04:35 -0300

    feat: change header when a table is selected

--- Mobile-PizzaHub/src/components/Header/index.tsx
+++ Mobile-PizzaHub/src/components/Header/index.tsx
@@ -1,13 +1,44 @@
+import { TouchableOpacity } from 'react-native';
 import { Text } from '../Text';
-import { Container } from './styles';
+import { Container, OrderContent, OrderHeader, Table } from './styles';
 
-export function Header(){
+interface HeaderProps{
+  selectedTable: string
+  onCancelOrder: () => void
+}
+
+export function Header({selectedTable, onCancelOrder}: HeaderProps){
   return(
     <Container>
-      <Text size={14} opacity={0.9}>Bem vindo(a) ao</Text>
-      <Text  size={24} weight="700">Waiter
-        <Text size={24}>App</Text>
-      </Text>
+      {!selectedTable
+        ? (
+          <>
+            <Text size={14} opacity={0.9}>Bem vindo(a) ao</Text>
+            <Text  size={24} weight="700">Waiter
+              <Text size={24}>App</Text>
+            </Text>
+          </>
+        )
+        : (
+          <OrderContent>
+            <OrderHeader>
+              <Text size={24} weight="600">
+                Pedido
+              </Text>
+
+              <TouchableOpacity onPress={onCancelOrder}>
+                <Text  color="#d73035" weight='600'size={14} >
+                Cancelar Pedido
+                </Text>
+              </TouchableOpacity>
+            </OrderHeader>
+
+            <Table>
+              <Text color='#666'>Mesa {selectedTable}</Text>
+            </Table>
+          </OrderContent>
+        )
+      }
     </Container>
   );
 }
