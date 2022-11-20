44159119abc734a2b4b332ee9a437dcba051ba3e
João Paulo Mesquita <jp-mesquita@live.com>
2022-11-20 13:42:55 -0300

    feat: allowing origins, methods and headers to api

--- server/src/index.ts
+++ server/src/index.ts
@@ -12,6 +12,12 @@ mongoose.connect(MONGODB_SECRET_CONECTION_KEY)
     const app = express();
     const port = 3333;
 
+    app.use((req, res, next) =>{
+      res.setHeader('Access-Control-Allow-Origin', '*');
+      res.setHeader('Access-Control-Allow-Methods', '*');
+      res.setHeader('Access-Control-Allow-Headers', '*');
+      next();
+    });
     app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
     app.use(express.json());
     app.use(router);
